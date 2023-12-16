785e043e8f7ea2169dd074118ceefc77f0f9efef
amit00986 <aryan.amit0824@gmail.com>
2023-12-16 11:48:17 +0530

    changes

--- server.js
+++ server.js
@@ -1,20 +1,13 @@
 require('module-alias/register');
 const express = require('express');
 const config = require('./src/config');
-const mongoose = require('mongoose'); // Import Mongoose
+const mongoose = require('mongoose');
 const cors = require('cors');
-
-const path = require('path');
 const http = require('http');
 const socketio = require('socket.io');
-const Message = require('./src/apis/models/MessageModel'); // Replace 'path-to-your-message-model' with the actual path
-const Chat = require('./src/apis/models/ChatModel'); // Replace 'path-to-your-chat-model' with the actual path
-
-const {
-  Logger,
-  ErrorHandler: { BaseError, INTERNAL_SERVER_ERROR, PAGE_NOT_FOUND_ERROR },
-} = require('intelli-utility');
-
+const Message = require('./src/apis/models/MessageModel');
+const Chat = require('./src/apis/models/ChatModel');
+const { Logger, ErrorHandler: { BaseError, INTERNAL_SERVER_ERROR, PAGE_NOT_FOUND_ERROR } } = require('intelli-utility');
 const InitApp = require('./src/InitApp');
 
 const router = require('./src/apis/routes');
@@ -22,12 +15,12 @@ const PORT = 5000;
 
 const app = express();
 app.use(cors());
+app.use(express.json());
 
 const server = http.createServer(app);
 const io = socketio(server);
 
-// Connect to MongoDB
-mongoose.connect('mongodb://127.0.0.1:27017/chat', {
+mongoose.connect('mongodb+srv://Intellie_dev_db:inteli123@cluster0.aycly9f.mongodb.net/chat?retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
@@ -35,56 +28,48 @@ mongoose.connect('mongodb://127.0.0.1:27017/chat', {
 io.on('connection', (socket) => {
   console.log('A user connected.');
 
-<<<<<<< HEAD
-  //Broadcast when a user connects
-=======
-  socket.on('createRoom',  (roomName , roomId) => {
-    // Create a new chat room in MongoDB
+  socket.on('createRoom', (roomName, roomId, creatorUserId) => {
     const newChat = new Chat({
       chatName: roomName,
-      chatId: roomId, // You can use the socket id as a unique identifier
+      chatId: roomId,
       isGroupChat: true,
-      users: [socket.id], // Initially, the creator is the only user
+      users: [creatorUserId],
     });
->>>>>>> e3622fd (chat socket)
 
     newChat.save((err, chat) => {
       if (!err) {
         socket.join(roomName);
+        io.emit('createRoom', roomName, roomId);
       } else {
         console.error('Error creating chat room:', err);
       }
     });
   });
 
-  socket.on('joinRoom', (roomName, userId ) => {
-    // Join an existing chat room
-    // console.log(roomName , userId)
+  socket.on('joinRoom', (roomName, userId) => {
     Chat.findOne({ chatId: roomName }, (err, chat) => {
       if (!err && chat) {
         chat.users.push(userId);
         chat.save();
         socket.join(roomName);
+        io.emit('joinRoom', roomName, userId);
       } else {
         console.error('Error joining chat room:', err);
       }
     });
   });
 
-  socket.on('sendMessage',  (roomId, message) => {
-    // Store the message in MongoDB
+  socket.on('sendMessage', (roomId, message) => {
     const newMessage = new Message({
-      senderId: socket.id, // You can use the socket id as the sender's ID
+      senderId: socket.id,
       sender_name: message.sender,
       content: message.text,
-      chatId: roomId, // Use roomName as the chatId
+      chatId: roomId,
     });
 
     try {
-       newMessage.save();
-      // Broadcast the message to everyone in the room
+      newMessage.save();
       io.to(roomId).emit('message', newMessage);
-      // console.log(newMessage)
     } catch (error) {
       console.error('Error saving message to MongoDB:', error);
     }
@@ -92,7 +77,6 @@ io.on('connection', (socket) => {
 
   socket.on('getChatHistory', async (roomId) => {
     try {
-      // Retrieve chat history from MongoDB
       const messages = await Message.find({ chatId: roomId }).exec();
       socket.emit('chatHistory', messages);
     } catch (error) {
@@ -104,32 +88,75 @@ io.on('connection', (socket) => {
     console.log('A user disconnected.');
   });
 });
-server.listen(PORT, () => console.log(`Server is Quannected to Port ${PORT}`));
 
-// InitApp(app).then(() => {
-//   app.use(config.node.pathPrefix, router);
+// API to create a new chat room
+app.post('/api/createRoom', (req, res) => {
+  const { roomName, roomId, creatorUserId } = req.body;
 
-//   app.use((req, res, next) => {
-//     next(new PAGE_NOT_FOUND_ERROR());
-//   });
+  const newChat = new Chat({
+    chatName: roomName,
+    chatId: roomId,
+    isGroupChat: true,
+    users: [creatorUserId],
+  });
 
-//   app.use(async (error, req, res, next) => {
-//     Logger.error({ error: error });
-//     try {
-//       if (!(error instanceof BaseError)) {
-//         throw new INTERNAL_SERVER_ERROR();
-//       } else throw error;
-//     } catch (err) {
-//       Logger.error('error inside chat-mngmt-service', err);
-//       console.log('error inside chat management service:\n', err);
-//       await err.handleError(req, res);
-//     }
-//   });
+  newChat.save((err, chat) => {
+    if (!err) {
+      io.emit('createRoom', roomName, roomId);
+      return res.status(201).json({ message: 'Chat room created successfully' });
+    } else {
+      console.error('Error creating chat room:', err);
+      return res.status(500).json({ message: 'Failed to create chat room' });
+    }
+  });
+});
 
-//   const serverInstance = app.listen(config.node.port, () => {
-//     Logger.info({ msg: `Base URL: http://${config.node.host}:${config.node.port}${config.node.pathPrefix}` });
-//   });
+// API to join an existing chat room
+app.post('/api/joinRoom', async (req, res) => {
+  try {
+    const { roomName, userId } = req.body;
 
-//   // If you're using this for unit testing, you can export the server instance
-//   // module.exports = serverInstance;
-// });
+    // Check if the user is already in the chat
+    const chat = await Chat.findOne({ chatId: roomName });
+
+    if (chat && chat.users.includes(userId)) {
+      return res.status(400).json({ message: 'User already added to the chat' });
+    }
+
+    // If not, proceed to add the user
+    if (chat) {
+      chat.users.push(userId);
+      await chat.save();
+      io.emit('joinRoom', roomName, userId);
+      return res.status(200).json({ message: 'Joined chat room successfully' });
+    } else {
+      console.error('Error joining chat room: Chat not found');
+      return res.status(404).json({ message: 'Chat room not found' });
+    }
+  } catch (error) {
+    console.error('Error joining chat room:', error);
+    return res.status(500).json({ message: 'Internal Server Error' });
+  }
+});
+
+// API to get all chats for a user
+app.get('/api/chats/:userId', async (req, res) => {
+  try {
+    const userId = req.params.userId;
+    
+    // Retrieve all chat documents for the specified userId
+    const chats = await Chat.find({ users: userId });
+
+    res.json({ success: true, data: chats });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ success: false, message: 'Internal Server Error' });
+  }
+});
+
+// ... (the rest of your existing code)
+
+// Start the server
+const serverInstance = app.listen(PORT, () => {
+  console.log(`Server is connected to Port ${PORT}`);
+});
